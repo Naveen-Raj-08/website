f99a94b3f53979c6414614709916b62a71aee1c2
Naveen raj.K <47858683+Naveen-Raj-08@users.noreply.github.com>
2021-11-21 20:48:09 +0530

    Modified the design changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
 
-    if (scroll > 100) {
+    if (scroll > 50) {
       $(".page-header").addClass("header-fixed");
       console.log(scroll);
     } else {
