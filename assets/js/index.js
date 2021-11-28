781f668fbfb81769d231cf9de46ebea5f4825095
Naveen raj.K <47858683+Naveen-Raj-08@users.noreply.github.com>
2021-11-28 17:01:49 +0530

    ripple effext and made header as fixed when its scrolled particular position

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,39 @@
 $(document).ready(function () {
+  var aboutPos = $(".about-section").offset().top;
+  console.log(aboutPos);
+
   $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
-
     if (scroll > 50) {
       $(".page-header").addClass("header-fixed");
-      console.log(scroll);
     } else {
       $(".page-header").removeClass("header-fixed");
-      console.log(scroll);
     }
   });
+});
+
+var button = document.querySelectorAll(".btn-action");
+
+button.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    var x = e.clientX - e.target.offsetLeft;
+    var y = e.clientY - e.target.offsetTop;
+    console.log(x + " " + y);
+    let rippleElement = document.createElement("span");
+    rippleElement.style.left = x + "px";
+    rippleElement.style.top = y + "px";
+
+    this.appendChild(rippleElement);
+
+    setTimeout(() => {
+      rippleElement.remove();
+    }, 1000);
+  });
+});
+
+var pageLoading = document.querySelector(".page-loading");
+window.addEventListener("load", function (e) {
+  setTimeout(() => {
+    pageLoading.style.display = "none";
+  }, 2000);
 });
